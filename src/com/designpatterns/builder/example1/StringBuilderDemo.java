c4fba4c0a6b461d221b08af358f78fcd6b3daf63
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2020-11-22 00:45:57 +0530

    removed unnecessary files

--- src/com/designpatterns/builder/example1/StringBuilderDemo.java
+++ src/com/designpatterns/builder/example1/StringBuilderDemo.java
@@ -3,16 +3,20 @@ package com.designpatterns.builder.example1;
 public class StringBuilderDemo {
 
     public static void main(String args[]) {
+
         String hello = "hello";
         System.out.println("<p>" + hello + "</p>");
 
         String[] words = { "hello", "world" };
         StringBuilder sb = new StringBuilder();
         sb.append("<ul>\n");
+
         for (String word : words) {
             sb.append(String.format("  <li>%s</li>\n", word));
         }
+
         sb.append("</ul>");
         System.out.println(sb);
     }
+
 }
